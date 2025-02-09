0558e26903337beceb41c9dc940209e1afdb6183
Shemsedin Amme <112874058+shemsedinamme@users.noreply.github.com>
2025-02-09 19:27:46 +0300

    Update and rename Project to index.js

---
+++ frontend/src/components/projectManagement/index.js
@@ -0,0 +1,13 @@
+// frontend/src/components/projectManagement/index.js
+
+export { default as ProjectLifecycle } from './ProjectLifecycle';
+
+export { default as ProjectMetadata } from './ProjectMetadata';
+
+export { default as ProjectShare } from './ProjectShare';
+
+export { default as ProjectWorkflow } from './ProjectWorkflow';
+
+export { default as ProjectCollaboration } from './ProjectCollaboration';
+
+export { default as ProjectCreate } from './ProjectCreate';
