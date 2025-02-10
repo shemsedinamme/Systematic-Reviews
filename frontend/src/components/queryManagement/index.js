4b2ec9cb34b6ee83756a06f106bc252098fd1789
Shemsedin Amme <112874058+shemsedinamme@users.noreply.github.com>
2025-02-10 21:11:39 +0300

    Update index.js

--- frontend/src/components/queryManagement/index.js
+++ frontend/src/components/queryManagement/index.js
@@ -1,7 +1,6 @@
 // frontend/src/components/queryManagement/index.js
 export { default as AutomatedQuery } from './AutomatedQuery';
 export { default as CitationManagement } from './CitationManagement';
-export { default as DatabaseSelect } from './DatabaseSelect';
-export { default as QueryTranslation } from './QueryTranslation';
-export { default as SearchQueryBuilder } from './SearchQueryBuilder';
+export { default as QueryTranslator } from './QueryTranslator';
+export { default as QueryBuilder } from './QueryBuilder';
 export { default as SearchResults } from './SearchResults';
