f0d1cc3864cebbbdcb84e6f4128e789b2503774e
Shemsedin Amme <112874058+shemsedinamme@users.noreply.github.com>
2025-02-10 21:17:44 +0300

    Update and rename QueryTranslator.jsx to queryTranslator.jsx

--- frontend/src/components/queryManagement/queryTranslator.jsx
+++ frontend/src/components/queryManagement/queryTranslator.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from './QueryTranslator.module.css';
+import styles from './queryTranslator.module.css';
 
 const QueryTranslator = () => {
   const [query, setQuery] = useState('');
